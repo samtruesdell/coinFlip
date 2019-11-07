ef8c665ed4fe4bbb2c114e7c6842a82b36845936
Truesdell <Samuel.Truesdell@mass.gov>
2019-11-07 13:28:15 -0500

    Get rid of overwrites to nflip and sleep -- too complicated.

--- make_runAll.R
+++ make_runAll.R
@@ -5,36 +5,11 @@
 # how we often write scripts. Didn't just copy and paste into a new document
 # because this way any changes that are made in the files are preserved.
 
-# Note that nFlip and sleepTime in runFlip.R are overwritten by this code
-# when translating to runAll.R
-
-
-# How many times to flip the code in this local simulation
-nFlipUpdate <- 4
-
-# Extra sleep time to emulate more complicated simulation (larger for
-# HPCC example)
-sleepTimeUpdate <- 30
 
 # Read in each of the files
 rPre <- readLines('runPre.R')
 rFlip <- readLines('runFlip.R')
 rPost <- readLines('runPost.R')
-
-
-## edit the nFlip and sleepTime variables
-
-# Identify the text used for assignment
-nFlipAssign <- 'nFlip <- '
-sleepTimeAssign <- 'sleepTime <- '
-
-# find the indices
-nFlipIdx <- grep(pattern = nFlipAssign, x = rFlip)
-sleepTimeIdx <- grep(pattern = sleepTimeAssign, x = rFlip)
-
-# make the change
-rFlip[nFlipIdx] <- paste(nFlipAssign, nFlipUpdate)
-rFlip[sleepTimeIdx] <- paste(sleepTimeAssign, sleepTimeUpdate)
 
 
 # Create an object combining the three scripts
